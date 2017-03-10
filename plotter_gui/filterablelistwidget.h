fa388b982a7727107cef95af0488537e4a66f9e8
Davide Faconti <davide.faconti@gmail.com>
2017-03-10 13:12:39 +0100

    Rebase from 'rpm/indigo/plotjuggler'

--- plotter_gui/filterablelistwidget.h
+++ plotter_gui/filterablelistwidget.h
@@ -4,42 +4,13 @@
 #include <QWidget>
 #include <QAction>
 #include <QListWidget>
+#include <QTableWidget>
 #include <QMouseEvent>
 
 namespace Ui {
 class FilterableListWidget;
 }
-/*
-class CustomListWidget: public QListWidget
-{
-    Q_OBJECT
-public:
-    CustomListWidget(QWidget *parent = 0): QListWidget(parent) {}
-private:
 
-    virtual void mouseMoveEvent(QMouseEvent *event) override
-    {
-        if (event->buttons() & Qt::LeftButton) {
-            int distance = (event->pos() - event).manhattanLength();
-            if (distance >= QApplication::startDragDistance())
-                performDrag();
-        }
-        QListWidget::mouseMoveEvent(event);
-    }
-
-    void performDrag()
-    {
-        //const QMimeData *mimeData = event->mimeData();
-        //Add(mimeData->text()); // probably the slot you defined for your Add button
-
-        QString resourceName (":/icons/resources/office_chart_lines.png");
-        QPixmap pixmap (resourceName);
-        QCursor cursor (pixmap);
-        setCursor(cursor);
-        QListWidget::dragEnterEvent(event);
-    }
-
-};*/
 
 class FilterableListWidget : public QWidget
 {
@@ -54,11 +25,15 @@ public:
 
     void clear();
 
-    void addItem(QListWidgetItem* item);
+    void addItem(QTableWidgetItem *item);
 
     void addItems(const QStringList& index_list);
 
-    QList<QListWidgetItem*> findItems(const QString& text) const;
+    QList<int> findRowsByName(const QString& text) const;
+
+    const QTableWidget *table() const;
+
+    QTableWidget *table();
 
 private slots:
 
@@ -72,12 +47,13 @@ private slots:
 
     void on_pushButtonSettings_toggled(bool checked);
 
+    void on_checkBoxHideSecondColumn_toggled(bool checked);
+
 private:
     Ui::FilterableListWidget *ui;
     QPoint _drag_start_pos;
 
-    const QListWidget* list() const;
-    QListWidget* list();
+
 
     bool eventFilter(QObject *object, QEvent *event);
 };
