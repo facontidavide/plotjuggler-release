6e08be3e5a954bea6b19a67efd1bf76ba79e1e16
Davide Faconti <davide.faconti@gmail.com>
2017-03-16 23:59:09 +0100

    Rebase from 'upstream'

--- plotter_gui/curvecolorpick.h
+++ plotter_gui/curvecolorpick.h
@@ -15,27 +15,29 @@ class CurveColorPick : public QDialog
     Q_OBJECT
 
 public:
-    explicit CurveColorPick(std::map<QString, QColor>* mapped_colors, QWidget *parent = 0);
+    explicit CurveColorPick(const std::map<QString, QColor>& mapped_colors, QWidget *parent = 0);
     ~CurveColorPick();
+
+    bool anyColorModified() const ;
 
 private slots:
     void on_pushButtonClose_clicked();
 
     void on_pushButtonApply_clicked();
 
-    void on_pushButtonReset_clicked();
-
     void on_listWidget_itemClicked(QListWidgetItem *item);
 
-    void on_pushButtonResetAll_clicked();
+
+signals:
+    void changeColor(QString, QColor);
 
 private:
     Ui::CurveColorPick *ui;
-    std::map<QString, QColor>  _prev_colors;
-    std::map<QString, QColor>* _colors_ptr;
-
     color_widgets::ColorWheel   *color_wheel;
     color_widgets::ColorPreview *color_preview;
+
+    const std::map<QString, QColor>& _mapped_colors;
+    bool _any_modified;
 };
 
 #endif // CURVECOLORPICK_H
