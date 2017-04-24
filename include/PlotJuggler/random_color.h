1707f1c864ba036de98ce069f1072229f69f2742
Davide Faconti <davide.faconti@gmail.com>
2017-04-24 18:37:38 +0200

    Rebase from 'debian/kinetic/plotjuggler'

--- include/PlotJuggler/random_color.h
+++ include/PlotJuggler/random_color.h
@@ -9,15 +9,15 @@ inline QColor randomColorHint()
     QColor color;
     switch( index%9 )
     {
-    case 0:  color = QColor(Qt::blue) ;break;
-    case 1:  color = QColor(Qt::darkGreen);break;
-    case 2:  color =  QColor(Qt::red); break;
-    case 3:  color =  QColor(Qt::magenta); break;
-    case 4:  color =  QColor(Qt::darkBlue); break;
-    case 5:  color =  QColor(Qt::darkCyan); break;
-    case 6:  color =  QColor(Qt::gray); break;
+    case 0:  color =  QColor(Qt::blue);       break;
+    case 1:  color =  QColor(Qt::darkGreen);  break;
+    case 2:  color =  QColor(Qt::red);        break;
+    case 3:  color =  QColor(Qt::magenta);    break;
+    case 4:  color =  QColor(Qt::darkBlue);   break;
+    case 5:  color =  QColor(Qt::darkCyan);   break;
+    case 6:  color =  QColor(Qt::gray);       break;
     case 7:  color =  QColor(Qt::darkYellow); break;
-    case 8:  color =  QColor(Qt::darkRed); break;
+    case 8:  color =  QColor(Qt::darkRed);    break;
     }
     index++;
     return color;
