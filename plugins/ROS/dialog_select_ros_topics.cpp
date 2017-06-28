cdbea3408f7ae861242010b83da0a8b27bd356cc
Davide Faconti <davide.faconti@gmail.com>
2017-06-28 15:30:26 +0200

    Rebase from 'rpm/indigo/plotjuggler'

--- plugins/ROS/dialog_select_ros_topics.cpp
+++ plugins/ROS/dialog_select_ros_topics.cpp
@@ -8,6 +8,7 @@
 #include <QTableWidgetItem>
 #include <QSettings>
 #include <QHeaderView>
+#include <QDebug>
 
 #include "dialog_select_ros_topics.h"
 #include "rule_editing.h"
@@ -51,17 +52,21 @@ DialogSelectRosTopics::DialogSelectRosTopics(const std::vector<std::pair<QString
 void DialogSelectRosTopics::updateTopicList(std::vector<std::pair<QString, QString> > topic_list)
 {
     // add if not present
-    for (int row=0; row< topic_list.size(); row++)
+    for (const auto& it: topic_list)
     {
-        const QString& topic_name = topic_list[row].first ;
-        const QString& type_name  = topic_list[row].second ;
+        const QString& topic_name = it.first ;
+        const QString& type_name  = it.second ;
 
-        QList<QTableWidgetItem *> same_name_items = ui->listRosTopics->findItems(topic_name, Qt::MatchExactly);
         bool found = false;
-        for (QTableWidgetItem* item: same_name_items)
+        for(int r=0; r < ui->listRosTopics->rowCount(); r++ )
         {
-            if( item->column() == 0){ found = true; }
+            const QTableWidgetItem *item = ui->listRosTopics->item(r,0);
+            if( item->text() == topic_name){
+                found = true;
+                break;
+            }
         }
+
         if( !found )
         {
             int new_row = ui->listRosTopics->rowCount();
@@ -70,17 +75,26 @@ void DialogSelectRosTopics::updateTopicList(std::vector<std::pair<QString, QStri
             // order IS important, don't change it
             ui->listRosTopics->setItem(new_row, 1, new QTableWidgetItem( type_name ));
             ui->listRosTopics->setItem(new_row, 0, new QTableWidgetItem( topic_name ));
-
-            if(_default_selected_topics.contains(topic_name) )
-            {
-                ui->listRosTopics->selectRow(new_row);
-            }
         }
     }
 
     ui->listRosTopics->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
     ui->listRosTopics->horizontalHeader()->setSectionResizeMode(1, QHeaderView::ResizeToContents);
     ui->listRosTopics->sortByColumn(0, Qt::AscendingOrder);
+
+    if( ui->listRosTopics->rowCount() == 1)
+    {
+        ui->listRosTopics->selectRow(0);
+    }
+    else{
+        for(int row=0; row < ui->listRosTopics->rowCount(); row++ )
+        {
+            const QTableWidgetItem *item = ui->listRosTopics->item(row,0);
+            if(_default_selected_topics.contains( item->text() ) ){
+                ui->listRosTopics->selectRow(row);
+            }
+        }
+    }
 }
 
 
