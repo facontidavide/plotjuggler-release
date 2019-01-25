94f2fb9b6275ede88ed685411874471f0e1d10ae
Davide Facont <davide.faconti@gmail.com>
2019-01-25 18:34:02 +0100

    Rebase from 'release/indigo/plotjuggler'

--- plugins/ROS/dialog_select_ros_topics.h
+++ plugins/ROS/dialog_select_ros_topics.h
@@ -22,9 +22,9 @@ public:
 
     explicit DialogSelectRosTopics(const std::vector<std::pair<QString,QString>>& topic_list,
                                    QStringList default_selected_topics,
-                                   QWidget *parent = 0);
+                                   QWidget *parent = nullptr);
 
-    ~DialogSelectRosTopics();
+    ~DialogSelectRosTopics() override;
 
     QStringList getSelectedItems();
 
@@ -33,6 +33,8 @@ public:
     const QCheckBox* checkBoxUseHeaderStamp();
 
     const QCheckBox *checkBoxUseRenamingRules();
+
+    bool discardEntireArrayIfTooLarge();
 
     QString prefix();
 
@@ -56,6 +58,8 @@ private slots:
 
     void on_lineEditFilter_textChanged(const QString &search_string);
 
+    void on_spinBoxArraySize_valueChanged(int value);
+
 private:
 
     void closeEvent(QCloseEvent *event) override;
@@ -70,6 +74,37 @@ private:
 
 };
 
+// SOME SFINAE magic....
+
+template <typename T>
+class has_setMaxArrayPolicy
+{
+    typedef char one;
+    typedef long two;
+
+    template <typename C> static one test( decltype(&C::setMaxArrayPolicy) ) ;
+    template <typename C> static two test(...);
+
+public:
+    enum { value = sizeof(test<T>(nullptr)) == sizeof(char) };
+};
+
+
+
 nonstd::optional<double> FlatContainerContainHeaderStamp(const RosIntrospection::RenamedValues& flat_container);
+
+template<class T, typename std::enable_if< has_setMaxArrayPolicy<T>::value, int>::type = 0>
+inline bool setMaxArrayPolicy(T* parser, bool policy)
+{
+    parser->setMaxArrayPolicy( policy );
+    return true;
+}
+
+template<class T, typename std::enable_if< !has_setMaxArrayPolicy<T>::value, int>::type = 0>
+inline bool setMaxArrayPolicy(T* , bool )
+{
+    return false;
+}
+
 
 #endif // DIALOG_SELECT_ROS_TOPICS_H
