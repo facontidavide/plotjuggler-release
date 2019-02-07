547fd34043e8cdaf89012ec910e3b5204d66d113
Davide Faconti <davide.faconti@eurecat.org>
2019-02-07 10:12:46 +0100

    Rebase from 'upstream'

--- plotter_gui/plotmagnifier.h
+++ plotter_gui/plotmagnifier.h
@@ -4,6 +4,7 @@
 #include <QTimer>
 #include <qwt_plot_magnifier.h>
 #include <qwt_plot.h>
+#include <QEvent>
 
 class PlotMagnifier : public QwtPlotMagnifier
 {
@@ -12,15 +13,19 @@ class PlotMagnifier : public QwtPlotMagnifier
 
 public:
     explicit PlotMagnifier( QWidget *canvas);
-    virtual ~PlotMagnifier();
+    virtual ~PlotMagnifier() override;
 
     void setAxisLimits(int axis,double lower, double upper);
+    virtual void rescale( double factor ) override;
+    virtual void widgetWheelEvent( QWheelEvent *event ) override;
 
 protected:
-    virtual void rescale( double factor ) override;
 
-    virtual void widgetWheelEvent( QWheelEvent *event ) override;
     virtual void widgetMousePressEvent( QMouseEvent* event ) override;
+//    virtual void widgetKeyPressEvent( QKeyEvent * event) override;
+//    virtual void widgetKeyReleaseEvent( QKeyEvent *event)  override;
+
+    bool eventFilter( QObject *obj, QEvent *event) override;
 
     double _lower_bounds[QwtPlot::axisCnt];
     double _upper_bounds[QwtPlot::axisCnt];
@@ -33,7 +38,8 @@ signals:
 private:
     QPointF invTransform(QPoint pos);
     QTimer _future_emit;
-
+    bool _x_pressed;
+    bool _y_pressed;
 };
 
 #endif // PLOTMAGNIFIER_H
