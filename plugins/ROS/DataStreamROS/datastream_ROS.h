a40fc8a768d35fa6e622ca61831062cff5d21944
Davide Faconti <davide.faconti@eurecat.org>
2019-03-07 11:15:39 +0100

    Rebase from 'debian/melodic/plotjuggler'

--- plugins/ROS/DataStreamROS/datastream_ROS.h
+++ plugins/ROS/DataStreamROS/datastream_ROS.h
@@ -8,7 +8,7 @@
 #include <topic_tools/shape_shifter.h>
 #include "PlotJuggler/datastreamer_base.h"
 #include <ros_type_introspection/ros_introspection.hpp>
-
+#include <rosgraph_msgs/Clock.h>
 
 class  DataStreamROS: public DataStreamer
 {
@@ -40,6 +40,8 @@ private:
 
     void topicCallback(const topic_tools::ShapeShifter::ConstPtr& msg, const std::string &topic_name);
 
+    void clockCallback(const rosgraph_msgs::Clock::ConstPtr& msg);
+
     bool _running;
 
     std::shared_ptr<ros::AsyncSpinner> _spinner;
@@ -51,12 +53,14 @@ private:
     std::string _prefix;
 
     ros::NodeHandlePtr _node;
+    ros::Subscriber _clock_subscriber;
     std::map<std::string, ros::Subscriber> _subscribers;
     RosIntrospection::SubstitutionRuleMap _rules;
 
     int _received_msg_count;
 
     QAction* _action_saveIntoRosbag;
+    QAction* _action_clearBuffer;
 
     std::map<std::string, int> _msg_index;
 
@@ -69,6 +73,8 @@ private:
     QTimer* _periodic_timer;
 
     bool _roscore_disconnection_already_notified;
+
+    ros::Time _clock_time;
 
     void timerCallback();
 
