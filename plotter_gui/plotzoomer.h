fc298e7dccc5e02ca65027de174e95d748d12acb
Davide Facont <davide.faconti@gmail.com>
2019-03-24 15:04:18 +0100

    Imported upstream version '2.1.8' of 'upstream'

--- plotter_gui/plotzoomer.h
+++ plotter_gui/plotzoomer.h
@@ -9,19 +9,28 @@ class PlotZoomer : public QwtPlotZoomer
 public:
     PlotZoomer();
 
-    explicit PlotZoomer( QWidget *, bool doReplot = true );
+    explicit PlotZoomer( QWidget *);
 
     virtual ~PlotZoomer() override = default;
+
+    void keepAspectratio(bool doKeep)
+    {
+        _keep_aspect_ratio = doKeep;
+    }
+
 protected:
     virtual void widgetMousePressEvent( QMouseEvent * event)  override;
     virtual void widgetMouseReleaseEvent( QMouseEvent * event) override;
     virtual void widgetMouseMoveEvent( QMouseEvent * event) override;
     virtual bool accept( QPolygon & ) const override;
 
+    virtual void zoom( const QRectF &rect ) override;
+
     virtual QSizeF minZoomSize() const override;
 private:
     bool _mouse_pressed;
     bool _zoom_enabled;
+    bool _keep_aspect_ratio;
     QPoint _initial_pos;
 };
 
