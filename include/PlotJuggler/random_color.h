d423e55b53a90b165feebaa9dec90a3cf5e1b3c4
Davide Faconti <davide.faconti@gmail.com>
2019-09-24 19:09:21 +0200

    Rebase from 'release/melodic/plotjuggler'

--- include/PlotJuggler/random_color.h
+++ include/PlotJuggler/random_color.h
@@ -7,17 +7,18 @@ inline QColor randomColorHint()
 {
     static int index = 0;
     QColor color;
-    switch( index%9 )
+   // https://matplotlib.org/3.1.1/users/dflt_style_changes.html
+    switch( index%8 )
     {
-    case 0:  color =  QColor("#1464a0");   break; //blue
-    case 1:  color =  QColor("#05740d");   break; //green
-    case 2:  color =  QColor("#ff1318");   break; //red
-    case 3:  color =  QColor("#f700f7");   break; //magenta
-    case 4:  color =  QColor("#0033ee");   break; //dark blue
-    case 5:  color =  QColor("#007777");   break; //dark cyan
-    case 6:  color =  QColor("#50b47f");   break; // green-ish
-    case 7:  color =  QColor("#f4531d");   break; //orange
-    case 8:  color =  QColor("#8e3488");   break; //volet
+    case 0:  color =  QColor("#1f77b4");   break;
+    case 1:  color =  QColor("#d62728");   break;
+    case 2:  color =  QColor("#1ac938");   break;
+    case 3:  color =  QColor("#ff7f0e");   break;
+
+    case 4:  color =  QColor("#f14cc1");   break;
+    case 5:  color =  QColor("#9467bd");   break;
+    case 6:  color =  QColor("#17becf");   break;
+    case 7:  color =  QColor("#bcbd22");   break;
     }
     index++;
     return color;
