cff3c62ee909c21e49fb52672a797bcae7094fec
Davide Faconti <davide.faconti@gmail.com>
2020-02-07 23:14:51 +0100

    Rebase from 'release/kinetic/plotjuggler'

---
+++ plotter_gui/new_release_dialog.cpp
@@ -0,0 +1,34 @@
+#include "new_release_dialog.h"
+#include "ui_new_release_dialog.h"
+#include <QSettings>
+#include <QDesktopServices>
+#include <QUrl>
+#include <QDialogButtonBox>
+
+NewReleaseDialog::NewReleaseDialog(QWidget *parent, QString release, QString title, QString url) :
+  QDialog(parent),
+  ui(new Ui::NewReleaseDialog)
+{
+  ui->setupUi(this);
+
+  connect(ui->pushButtonWeb, &QPushButton::clicked, this,
+          [=]{ QDesktopServices::openUrl( QUrl(url)); });
+
+  connect(ui->buttonBox, &QDialogButtonBox::rejected, this, [=]
+          {
+            if( ui->dontShowAgain->isChecked() )
+            {
+              QSettings settings;
+              settings.setValue("NewRelease/dontShowThisVersion", release);
+            }
+          });
+
+  ui->labelRelease->setText(release);
+  ui->labelTitle->setText(title);
+}
+
+NewReleaseDialog::~NewReleaseDialog()
+{
+  delete ui;
+}
+
