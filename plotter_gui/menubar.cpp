ea0b6717721e5d2363210ad05fb82404f0683c54
Davide Faconti <davide.faconti@gmail.com>
2020-02-07 19:29:55 +0100

    Rebase from 'debian/melodic/plotjuggler'

--- plotter_gui/menubar.cpp
+++ plotter_gui/menubar.cpp
@@ -1,38 +1,39 @@
 #include "menubar.h"
 #include <QDebug>
 #include <QFontDatabase>
-#include <QPainter>
 #include <QFont>
 #include <QColor>
 
+MenuBar::MenuBar(QWidget *parent): QMenuBar(parent),
+                                     _painter(this)
+{
+
+  int font_id = QFontDatabase::addApplicationFont("://resources/DejaVuSans-ExtraLight.ttf");
+  QString family = QFontDatabase::applicationFontFamilies(font_id).at(0);
+  QFont font(family);
+  font.setStyleStrategy(QFont::PreferAntialias);
+
+  _painter.setFont( QFont(family, 12) );
+  _width_plot = _painter.fontMetrics().width("Plot");
+  _width_juggler = _painter.fontMetrics().width("Juggler");
+}
+
 void MenuBar::paintEvent(QPaintEvent *event)
 {
-    QMenuBar::paintEvent(event);
+  QMenuBar::paintEvent(event);
 
-    int font_id = QFontDatabase::addApplicationFont("://resources/DejaVuSans-ExtraLight.ttf");
-    QString family = QFontDatabase::applicationFontFamilies(font_id).at(0);
-    QFont font(family);
-    font.setStyleStrategy(QFont::PreferAntialias);
-
-    QPainter painter(this);
-    painter.setFont( QFont(family, 12) );
-
-    int width_plot = painter.fontMetrics().width("Plot");
-    int width_juggler = painter.fontMetrics().width("Juggler");
-    int text_width = width_plot + width_juggler;
-    {
-        QPoint topleft( this->rect().width() - text_width - 12 , 0);
-        QSize rect_size( width_plot, this->rect().height() );
-        painter.setPen( QColor("#ce0e73"));
-        painter.drawText( QRect(topleft, rect_size),
-                          Qt::AlignHCenter | Qt::AlignVCenter, "Plot" );
-    }
-    {
-        QPoint topleft( this->rect().width() - width_juggler - 10 , 0);
-        QSize rect_size( width_juggler, this->rect().height() );
-        painter.setPen( QColor("#1b72cf"));
-        painter.drawText( QRect(topleft, rect_size),
-                          Qt::AlignHCenter | Qt::AlignVCenter, "Juggler" );
-    }
+  int text_width = _width_plot + _width_juggler;
+  {
+    QPoint topleft(this->rect().width() - text_width - 12, 0);
+    QSize rect_size(_width_plot, this->rect().height());
+    _painter.setPen(QColor("#ce0e73"));
+    _painter.drawText(QRect(topleft, rect_size), Qt::AlignHCenter | Qt::AlignVCenter, "Plot");
+  }
+  {
+    QPoint topleft(this->rect().width() - _width_juggler - 10, 0);
+    QSize rect_size(_width_juggler, this->rect().height());
+    _painter.setPen(QColor("#1b72cf"));
+    _painter.drawText(QRect(topleft, rect_size), Qt::AlignHCenter | Qt::AlignVCenter, "Juggler");
+  }
 }
 
