68f91d51eb7ade767a0c5c02278c9e45322b081a
Davide Faconti <daf@blue-ocean-robotics.com>
2020-05-24 10:48:36 +0200

    Rebase from 'upstream'

--- plotter_gui/transforms/transform_selector.h
+++ plotter_gui/transforms/transform_selector.h
@@ -3,34 +3,34 @@
 
 #include <QDialog>
 
-namespace Ui {
+namespace Ui
+{
 class transform_selector;
 }
 
 class TransformSelector : public QDialog
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    TransformSelector(QStringList builtin_transform, QStringList available_transforms, QString* default_tansform,
-                      std::map<std::string, QString> *curve_transforms,
-                      QWidget *parent = nullptr);
-    ~TransformSelector();
+  TransformSelector(QStringList builtin_transform, QStringList available_transforms, QString* default_tansform,
+                    std::map<std::string, QString>* curve_transforms, QWidget* parent = nullptr);
+  ~TransformSelector();
 
 private slots:
 
-    void on_buttonApplyDefault_clicked();
+  void on_buttonApplyDefault_clicked();
 
-    void on_buttonResetAll_clicked();
+  void on_buttonResetAll_clicked();
 
-    void on_transform_selector_accepted();
+  void on_transform_selector_accepted();
 
 private:
-    Ui::transform_selector *ui;
+  Ui::transform_selector* ui;
 
-    std::map<std::string, QString> *_curves_trans;
+  std::map<std::string, QString>* _curves_trans;
 
-    QString* _default_trans;
+  QString* _default_trans;
 };
 
-#endif // TRANSFORM_SELECTOR_H
+#endif  // TRANSFORM_SELECTOR_H
