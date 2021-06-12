9792b76cce306c90b8b141c2b97421e5344a5341
Davide Faconti <davide.faconti@gmail.com>
2021-06-12 18:30:57 +0200

    Imported upstream version '3.2.0' of 'upstream'

---
+++ plotjuggler_app/utils.h
@@ -0,0 +1,52 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QObject>
+#include "PlotJuggler/plotdata.h"
+
+using namespace PJ;
+
+class MonitoredValue : public QObject
+{
+  Q_OBJECT
+public:
+  MonitoredValue(QObject* parent = nullptr) : QObject(parent), _value(0)
+  {
+  }
+
+  void set(double newValue)
+  {
+    double prev = _value;
+    _value = newValue;
+    if (fabs(newValue - prev) > std::numeric_limits<double>::epsilon())
+    {
+      emit valueChanged(_value);
+    }
+  }
+
+  double get() const
+  {
+    return _value;
+  }
+signals:
+  void valueChanged(double);
+
+private:
+  double _value;
+};
+
+
+struct MoveDataRet
+{
+  std::vector<std::string> added_curves;
+  bool curves_updated = false;
+  bool data_pushed = false;
+};
+
+
+MoveDataRet MoveData(PlotDataMapRef& source,
+            PlotDataMapRef& destination,
+            bool remove_older);
+
+
+#endif  // UTILS_H
