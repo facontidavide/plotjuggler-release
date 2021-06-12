d44342806f65028db539d1aae53bfe4f44037373
Davide Faconti <davide.faconti@gmail.com>
2021-06-12 18:58:27 +0200

    Rebase from 'debian/noetic/plotjuggler'

---
+++ plotjuggler_app/dummy_data.cpp
@@ -0,0 +1,82 @@
+#include "dummy_data.h"
+#include <QColor>
+
+using namespace PJ;
+
+void BuildDummyData(PlotDataMapRef& datamap)
+{
+  size_t SIZE = 1000;
+  QStringList words_list;
+  words_list << "world/siam"
+             << "world/tre"
+             << "walk/piccoli"
+             << "walk/porcellin"
+             << "fly/high/mai"
+             << "fly/high/nessun"
+             << "fly/low/ci"
+             << "fly/low/dividera"
+             << "data_1"
+             << "data_2"
+             << "data_3"
+             << "data_10";
+
+  for (int i = 0; i < 100; i++)
+  {
+    words_list.append(QString("data_vect/%1").arg(i));
+  }
+
+  for (const QString& name : words_list)
+  {
+    double A = 6 * ((double)rand() / (double)RAND_MAX) - 3;
+    double B = 3 * ((double)rand() / (double)RAND_MAX);
+    double C = 3 * ((double)rand() / (double)RAND_MAX);
+    double D = 20 * ((double)rand() / (double)RAND_MAX);
+
+    auto it = datamap.addNumeric(name.toStdString());
+    PlotData& plot = it->second;
+
+    double t = 0;
+    for (unsigned indx = 0; indx < SIZE; indx++)
+    {
+      t += 0.01;
+      plot.pushBack(PlotData::Point(t + 35, A * sin(B * t + C) + D * t * 0.02));
+    }
+  }
+
+  //-------------------------------
+  auto tcGroup = datamap.getOrCreateGroup("myGroup/subGroup");
+  tcGroup->setAttribute("TextColor", QColor(Qt::blue) );
+  tcGroup->setAttribute("ToolTip", QString("This is a group") );
+
+  auto& tc_default = datamap.addNumeric("color/default", tcGroup)->second;
+  auto& tc_red = datamap.addNumeric("color/red", tcGroup)->second;
+
+  tc_red.setAttribute("TextColor", QColor(Qt::red) );
+
+  PlotData& sin_plot = datamap.addNumeric("_sin")->second;
+  PlotData& cos_plot = datamap.addNumeric("_cos")->second;
+  StringSeries& str_plot = datamap.addStringSeries("str_value")->second;
+
+  sin_plot.setAttribute("ToolTip", QString("sine") );
+  cos_plot.setAttribute("ToolTip", QString("cosine") );
+  str_plot.setAttribute("ToolTip", QString("this is a string") );
+
+  sin_plot.setAttribute("Italic", true);
+
+  //--------------------------------
+  double t = 0;
+  for (unsigned indx = 0; indx < SIZE; indx++)
+  {
+    t += 0.01;
+    sin_plot.pushBack(PlotData::Point(t + 20, sin(t * 0.4)));
+    cos_plot.pushBack(PlotData::Point(t + 20, cos(t * 0.4)));
+    tc_default.pushBack(PlotData::Point(t + 20, indx));
+    tc_default.pushBack(PlotData::Point(t + 20, indx));
+
+    switch( indx%3 ){
+    case 0: str_plot.pushBack( {t + 20, "Blue"} ); break;
+    case 1: str_plot.pushBack( {t + 20, "Red"} ); break;
+    case 2: str_plot.pushBack( {t + 20, "Green"} ); break;
+    }
+  }
+}
---
+++ plotjuggler_app/point_series_xy.h
@@ -0,0 +1,42 @@
+#ifndef POINT_SERIES_H
+#define POINT_SERIES_H
+
+#include "timeseries_qwt.h"
+
+class PointSeriesXY : public QwtSeriesWrapper
+{
+public:
+  PointSeriesXY(const PlotData* x_axis, const PlotData* y_axis);
+
+  virtual QPointF sample(size_t i) const override
+  {
+    const auto& p = _cached_curve.at(i);
+    return QPointF(p.x, p.y);
+  }
+
+  size_t size() const override;
+
+  std::optional<QPointF> sampleFromTime(double t) override;
+
+  RangeOpt getVisualizationRangeY(Range range_X) override;
+
+  bool updateCache(bool reset_old_data) override;
+
+  RangeOpt getVisualizationRangeX() override;
+
+  const PlotData* dataX() const
+  {
+    return _x_axis;
+  }
+  const PlotData* dataY() const
+  {
+    return _y_axis;
+  }
+
+protected:
+  const PlotData* _x_axis;
+  const PlotData* _y_axis;
+  PlotDataXY _cached_curve;
+};
+
+#endif  // POINT_SERIES_H
