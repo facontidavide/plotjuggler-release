dbda8c57b7beb314fecfe3734111b1362be60380
Davide Faconti <davide.faconti@gmail.com>
2021-09-07 20:30:53 +0200

    Rebase from 'upstream'

---
+++ plotjuggler_plugins/ToolboxQuaternion/toolbox_quaternion.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <QtPlugin>
+#include <thread>
+#include "PlotJuggler/toolbox_base.h"
+#include "PlotJuggler/plotwidget_base.h"
+#include "quaternion_to_rpy.h"
+
+namespace Ui {
+class quaternion_to_RPY;
+}
+
+class ToolboxQuaternion : public PJ::ToolboxPlugin
+{
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID "facontidavide.PlotJuggler3.Toolbox")
+  Q_INTERFACES(PJ::ToolboxPlugin)
+
+public:
+  ToolboxQuaternion();
+
+  ~ToolboxQuaternion() override;
+
+  const char* name() const override
+  {
+    return "Quaternion to RPY";
+  }
+
+  void init( PJ::PlotDataMapRef& src_data,
+             PJ::TransformsMap& transform_map) override;
+
+  std::pair<QWidget*, WidgetType> providedWidget() const override;
+
+public slots:
+
+  bool onShowWidget() override;
+
+private slots:
+
+  void on_pushButtonSave_clicked();
+
+  void onParametersChanged();
+
+  void onClosed();
+
+private:
+  QWidget* _widget;
+  Ui::quaternion_to_RPY *ui;
+
+  bool eventFilter(QObject *obj, QEvent *event) override;
+
+  QString _dragging_curve;
+
+  void autoFill(QString prefix);
+
+  PJ::PlotWidgetBase* _plot_widget = nullptr;
+
+  PJ::PlotDataMapRef* _plot_data = nullptr;
+
+  PJ::TransformsMap* _transforms = nullptr;
+
+  enum GenerateType{
+    PREVIEW,
+    SAVE
+  };
+
+  void generateRPY(GenerateType type);
+
+  std::vector<const PlotData*> getSrcData();
+};
+
