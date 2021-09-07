7c3ea4c8147afad04dd811d7171de2fa682a5ae2
Davide Faconti <davide.faconti@gmail.com>
2021-09-07 20:40:58 +0200

    Rebase from 'release/rolling/plotjuggler'

---
+++ plotjuggler_base/src/timeseries_qwt.h
@@ -0,0 +1,90 @@
+#ifndef TIMESERIES_QWT_H
+#define TIMESERIES_QWT_H
+
+#include "qwt_series_data.h"
+#include "PlotJuggler/plotdata.h"
+#include "PlotJuggler/transform_function.h"
+
+using namespace PJ;
+
+
+// wrapper to Timeseries inclduing a time offset
+class QwtSeriesWrapper: public QwtSeriesData<QPointF>
+{
+private:
+  const PlotDataXY* _data;
+  double _time_offset;
+
+public:
+  QwtSeriesWrapper(const PlotDataXY* data): _data(data), _time_offset(0.0) {}
+
+  QPointF sample(size_t i) const override;
+
+  void setTimeOffset(double offset);
+
+  virtual bool updateCache(bool reset_old_data) = 0;
+
+  size_t size() const override;
+
+  QRectF boundingRect() const override;
+
+  const PlotDataXY* plotData() const;
+
+  virtual RangeOpt getVisualizationRangeX();
+
+  virtual RangeOpt getVisualizationRangeY(Range range_X) = 0;
+
+  virtual std::optional<QPointF> sampleFromTime(double t) = 0;
+
+};
+
+
+class QwtTimeseries : public QwtSeriesWrapper
+{
+public:
+  QwtTimeseries(const PlotData* data):
+    QwtSeriesWrapper(data),
+    _ts_data(data)
+  {
+  }
+
+  virtual RangeOpt getVisualizationRangeY(Range range_X) override;
+
+  virtual std::optional<QPointF> sampleFromTime(double t) override;
+
+protected:
+  const PlotData* _ts_data;
+};
+
+//------------------------------------
+
+class TransformedTimeseries : public QwtTimeseries
+{
+public:
+  TransformedTimeseries(const PlotData* source_data);
+
+  TransformFunction::Ptr transform();
+
+  void setTransform(QString transform_ID);
+
+  virtual bool updateCache(bool reset_old_data) override;
+
+  QString transformName();
+
+  QString alias() const;
+
+  void setAlias(QString alias);
+
+protected:
+
+  QString _alias;
+  PlotData _dst_data;
+  const PlotData* _src_data;
+  TransformFunction_SISO::Ptr _transform;
+};
+
+//---------------------------------------------------------
+
+
+
+#endif  // PLOTDATA_H
