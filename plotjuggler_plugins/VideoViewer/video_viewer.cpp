683bafcfa7568563a3ba0b9e50f4f09bd91c36cb
Davide Faconti <davide.faconti@gmail.com>
2021-12-29 15:01:17 +0100

    Rebase from 'rpm/rolling/plotjuggler'

---
+++ plotjuggler_plugins/VideoViewer/video_viewer.cpp
@@ -0,0 +1,113 @@
+#include <QTextStream>
+#include <QFile>
+#include <QMessageBox>
+#include <QSettings>
+#include "video_viewer.h"
+
+PublisherVideo::PublisherVideo()
+{
+  _dialog = new VideoDialog(nullptr);
+  connect(_dialog, &VideoDialog::closed, this, [this]()
+          {
+            setEnabled(false);
+            emit closed();
+          });
+}
+
+PublisherVideo::~PublisherVideo()
+{
+  delete _dialog;
+}
+
+void PublisherVideo::updateState(double current_time)
+{
+  if(_dialog->isHidden())
+  {
+    return;
+  }
+  QString ref_curve = _dialog->referenceCurve();
+  if(ref_curve.isEmpty())
+  {
+    return;
+  }
+  auto it = _datamap->numeric.find(ref_curve.toStdString());
+  if( it == _datamap->numeric.end() )
+  {
+    return;
+  }
+  const auto& data = it->second;
+  auto position = data.getYfromX(current_time);
+  if( position )
+  {
+    if( !_dialog->isPaused() )
+    {
+      _dialog->pause(true);
+    }
+    _dialog->seekByValue(position.value());
+  }
+}
+
+void PublisherVideo::play(double current_time)
+{
+  updateState(current_time);
+}
+
+bool PublisherVideo::xmlSaveState(QDomDocument &doc, QDomElement &parent_element) const
+{
+  QDomElement config = doc.createElement("config");
+  config.setAttribute("video_file", _dialog->ui->lineFilename->text());
+  config.setAttribute("curve_name", _dialog->ui->lineEditReference->text());
+  config.setAttribute("use_frame", _dialog->ui->radioButtonFrame->isChecked() ? "true" : "false");
+
+  parent_element.appendChild(config);
+  return true;
+}
+
+bool PublisherVideo::xmlLoadState(const QDomElement &parent_element)
+{
+  QDomElement config = parent_element.firstChildElement("config");
+  if( config.isNull() )
+  {
+    return false;
+  }
+  _dialog->loadFile( config.attribute("video_file") );
+  _dialog->ui->lineEditReference->setText( config.attribute("curve_name") );
+  if( config.attribute("use_frame") == "true" )
+  {
+    _dialog->ui->radioButtonFrame->setChecked(true);
+  }
+  else{
+    _dialog->ui->radioButtonTime->setChecked(true);
+  }
+  _xml_loaded = true;
+  return true;
+}
+
+void PublisherVideo::setEnabled(bool enabled)
+{
+  QSettings settings;
+  auto ui = _dialog->ui;
+  if(enabled)
+  {
+    if( !_xml_loaded )
+    {
+      QString filename = settings.value("VideoDialog::video_file", "").toString();
+      if(filename != ui->lineFilename->text())
+      {
+        _dialog->loadFile(filename);
+      }
+
+      auto curve_name = settings.value("VideoDialog::curve_name", "").toString();
+      ui->lineEditReference->setText(curve_name);
+      _dialog->restoreGeometry(settings.value("VideoDialog::geometry").toByteArray());
+    }
+    _dialog->show();
+  }
+  else
+  {
+    settings.setValue("VideoDialog::video_file", ui->lineFilename->text());
+    settings.setValue("VideoDialog::curve_name", ui->lineEditReference->text());
+    settings.setValue("VideoDialog::geometry", _dialog->saveGeometry());
+    _dialog->hide();
+  }
+}
---
+++ plotjuggler_plugins/VideoViewer/video_viewer.h
@@ -0,0 +1,52 @@
+#ifndef STATE_PUBLISHER_VIDEO_VIEWER_H
+#define STATE_PUBLISHER_VIDEO_VIEWER_H
+
+#include <QObject>
+#include <QtPlugin>
+#include <zmq.hpp>
+#include <thread>
+#include <mutex>
+#include "PlotJuggler/statepublisher_base.h"
+#include "video_dialog.h"
+
+class PublisherVideo : public PJ::StatePublisher
+{
+  Q_OBJECT
+  Q_PLUGIN_METADATA(IID "facontidavide.PlotJuggler3.StatePublisher")
+  Q_INTERFACES(PJ::StatePublisher)
+
+public:
+  PublisherVideo();
+
+  virtual ~PublisherVideo();
+
+  const char* name() const override
+  {
+    return "Video Viewer";
+  }
+
+  bool enabled() const override
+  {
+    return _enabled;
+  }
+
+  void updateState(double current_time) override;
+
+  void play(double current_time) override;
+
+  bool xmlSaveState(QDomDocument& doc, QDomElement& parent_element) const override;
+
+  bool xmlLoadState(const QDomElement& parent_element) override;
+
+public slots:
+  virtual void setEnabled(bool enabled) override;
+
+private:
+
+  bool _enabled = false;
+  bool _xml_loaded = false;
+
+  VideoDialog* _dialog = nullptr;
+};
+
+#endif  // STATE_PUBLISHER_VIDEO_VIEWER_H
