71405a88472a9435886dc53c720deeacde472ab5
Davide Faconti <davide.faconti@gmail.com>
2022-06-29 12:18:25 +0200

    Rebase from 'upstream'

--- plotjuggler_app/transforms/lua_custom_function.h
+++ plotjuggler_app/transforms/lua_custom_function.h
@@ -26,11 +26,18 @@ public:
 
   bool xmlLoadState(const QDomElement& parent_element) override;
 
+  std::string getError(sol::error err);
+
 private:
   sol::state _lua_engine;
   sol::protected_function _lua_function;
   std::vector<double> _chan_values;
   std::mutex mutex_;
+  int global_lines_ = 0;
+  int function_lines_ = 0;
 };
+
+
+
 
 #endif  // LUA_CUSTOM_FUNCTION_H
